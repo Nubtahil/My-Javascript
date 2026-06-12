f7b19dcf4802c23cb81b7da0a8679bb7d3c64de4
Nubtahil Mustafa <nubtahilmustafa@gmail.com>
2026-06-12 13:41:46 +0500

    my js

---
+++ operators.js
@@ -0,0 +1,30 @@
+//Assignment operator
+let x = 20
+let y 
+y = x
+console.log(y)
+
+//compound operator
+let x = 20
+let u = 45
+// x = x + u
+
+// x -= u
+// x *= u
+// x %= u -- remainder
+// x /= u -- division
+
+console.log(x)
+
+//arithimetic operator
+let x = 20
+let u = 45
+// x = x + u
+// x -= u
+// x *= u
+// x /= u
+// x %= u
+//x ** 2 -- power of 2
+//u++ -- increment
+
+console.log(u)
